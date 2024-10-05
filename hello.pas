6911b9f9ad5b045a7dd912e3b07481a1af84216e
Kirill Sidorov <kir.sid06@gmail.com>
2024-10-05 19:44:10 +0300

    New program

--- hello.pas
+++ hello.pas
@@ -1,2 +1,10 @@
-﻿##
-Print('Hello world');
+﻿begin
+  var n := ReadInteger('Какой сейчас час:');
+  assert((n >= 0) and (n <= 23));
+  case n of
+    4..10: Print('Добрый утро, мир!');
+    11..16: Print('Добрый день, мир!');
+    17..22: Print('Добрый вечер, мир!');
+    23,0..3: Print('Доброй ночи, мир!');
+  end;
+end.
