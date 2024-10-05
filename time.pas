47f3f56f493678a8c195ae8446ac9ce1363e0eb7
Kirill Sidorov <kir.sid06@gmail.com>
2024-10-05 20:11:40 +0300

    задание 2

---
+++ time.pas
@@ -0,0 +1,6 @@
+﻿begin
+  var Year := ReadInteger('Введите год:');
+  if  (year mod 4 = 0) and (year mod  100 <> 0) or (year mod 400 = 0) then
+    print('високосный')
+  else Print('не високосный');
+end.
